611cfc08c3d568b13f60c602f82ea828f3652916
AlexanderSayner <alexandr.deffirst@gmail.com>
2020-05-31 22:37:56 +0400

    SecondProgram

--- metrics/main.cpp
+++ metrics/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+#include <vector>
 
 using namespace std;
 
@@ -8,7 +9,7 @@ using namespace std;
  * @param mode 0 - ручной ввод, 1 - автоматический
  * @return 0 - выполнено без ошибок
  */
-int first(int mode) {
+int firstProgram(int mode) {
     auto fileName = "database.txt";
 
     // Заполнение файла
@@ -21,15 +22,14 @@ int first(int mode) {
     string color;
     char isBlossom = 'y';
 
-    string buffer = "";
+    string buffer;
     if (mode) {
         name = "Cactus";
         color = "Green";
-        isBlossom = 'y';
         buffer = name + "_" + color + "_" + isBlossom + "|";
         name = "Rose";
         color = "Red";
-        buffer = buffer + name + "_" + color + "_" + isBlossom + "\n";
+        buffer = buffer + name + "_" + color + "_" + isBlossom + "|";
     } else {
         bool isOneMore = true;
         while (isOneMore) {
@@ -39,7 +39,7 @@ int first(int mode) {
             cin >> color;
             cout << "Is it blossom? y/n" << '\n';
             cin >> isBlossom;
-            buffer = buffer + name + "_" + color + "_" + isBlossom + "|";
+            buffer += name + "_" + color + "_" + isBlossom + "|";
             cout << "Do you want write one more? Y/n";
             char answer;
             cin >> answer;
@@ -65,8 +65,7 @@ int first(int mode) {
         return -1;
     }
     string s;
-    for (infile >> s; !infile.eof(); infile >> s)
-        cout << s << endl;
+    infile >> s;
     infile.close();
     cout << "There are file records:\n" << s << '\n';
 
@@ -78,11 +77,11 @@ int first(int mode) {
     bool isFound = false;
     for (char i : s) {
 
-        buffer = buffer + i;
+        buffer += i;
         if (buffer == search && !isFound) {
             isFound = true;
         }
-        if (isFound && (i == '|' || i == s[s.length() - 1])) {
+        if (isFound && i == '|') {
             cout << "Got it:\n" << buffer;
             break;
         }
@@ -100,7 +99,105 @@ int first(int mode) {
  * @param mode 0 - ручной ввод, 1 - автоматический
  * @return 0 - выполнено без ошибок
  */
-int second(int mode) {
+int secondProgram(int mode) {
+    auto *fileName = "database.txt";
+
+    // Заполнение файла
+    ofstream outfile(fileName);
+    if (!outfile) {
+        cout << "Can't create file " << *fileName;
+        return -1;
+    }
+    auto *name = new string();
+    auto *color = new string();
+    char *isBlossom = new char('y');
+
+    auto *buffer = new string("");
+    if (mode) {
+        *name = "Cactus";
+        *color = "Green";
+        *buffer = *name + "_" + *color + "_" + *isBlossom + "|";
+        *name = "Rose";
+        *color = "Red";
+        *buffer = *buffer + *name + "_" + *color + "_" + *isBlossom + "|";
+    } else {
+        bool *isOneMore = new bool(true);
+        while (*isOneMore) {
+            cout << "Flower Name, please:" << '\n';
+            cin >> *name;
+            cout << "Flower color is:" << '\n';
+            cin >> *color;
+            cout << "Is it blossom? y/n" << '\n';
+            cin >> *isBlossom;
+            *buffer = *buffer + *name + "_" + *color + "_" + *isBlossom + "|";
+            cout << "Do you want write one more? Y/n";
+            char *answer;
+            cin >> *answer;
+            switch (*answer) {
+                case 'Y':
+                case 'y': {
+                    *isOneMore = true;
+                    break;
+                }
+                default:
+                    *isOneMore = false;
+                    break;
+            }
+        }
+        delete isOneMore;
+    }
+    outfile << *buffer << endl;
+    outfile.close();
+
+    // Считываение файла
+    ifstream infile(fileName, ios::binary);
+    if (!infile.is_open()) {
+        cout << "Can't open file " << *fileName;
+        return -1;
+    }
+    infile.seekg(0, ios::end);
+    int n = infile.tellg();
+    infile.seekg(0, ios::beg);
+    char *s = new char[n + 1];
+    s[n] = 0;
+    infile.read(s, n);
+    cout << "There are file records:\n" << s;
+
+    // Поиск по файлу
+    cout << "Searching by name:\n" << "Please, enter the flowers name:\n";
+    string input;
+    cin >> input;
+    string *search = new string(input);
+    delete buffer;
+    buffer = new string("");
+    bool *isFound = new bool(false);
+
+    for (char *it = s; *it; ++it) {
+        string *buf = new string(*buffer + *it);
+        delete buffer;
+        buffer = new string(*buf);
+
+        if (*buffer == *search && !*isFound) {
+            *isFound = true;
+        }
+        if (*isFound && *it == '|') {
+            cout << "Got it:\n" << *buffer;
+            break;
+        }
+
+        if (*it == '|') {
+            delete buffer;
+            buffer = new string("");
+        }
+    }
+
+    free(name);
+    free(color);
+    free(isBlossom);
+    free(buffer);
+    free(search);
+    free(s);
+    free(isFound);
     return 0;
 }
 
@@ -109,11 +206,12 @@ int second(int mode) {
  * @param mode 0 - ручной ввод, 1 - автоматический
  * @return 0 - выполнено без ошибок
  */
-int third(int mode) {
+int thirdProgram(int mode) {
     return 0;
 }
 
 int main() {
-    first(1);
+//    firstProgram(1);
+    secondProgram(1);
     return 0;
 }
